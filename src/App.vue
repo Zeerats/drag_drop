d75d48cd3fd162a6e1e7971cf22c9ed9545d6191
Zeerats <scbmediasolutions@gmail.com>
2022-04-15 23:44:51 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,104 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flexbox">
+    <div 
+    class="dropzone"
+    @drop="onDrop($event, 1)"
+    @dragenter.prevent
+    @dragover.prevent
+    >
+      <div v-for="item in getList(1)" 
+      :key="item.id" 
+      class="drag-el"
+      draggable="true"
+      @dragstart="startDrag($event, item)"
+      >
+        {{ item.title }}
+      </div>
+    </div>
+    <div 
+    class="dropzone"
+    @drop="onDrop($event, 2)"
+    @dragenter.prevent
+    @dragover.prevent
+    >
+      <div v-for="item in getList(2)" 
+      :key="item.id" 
+      class="drag-el"
+      draggable="true"
+      @dragstart="startDrag($event, item)"
+      >
+        {{ item.title }}
+      </div>
+    </div>
+    <div @click="addList" class="dropzone">ADD LIST</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from 'vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const items = ref([
+      { id: 0, title: 'Item A', list: 1},
+      { id: 1, title: 'Item B', list: 1},
+      { id: 2, title: 'Item C', list: 2}
+    ])
+
+    const getList = (list) => {
+      return items.value.filter((item) => item.list == list)
+    }
+
+    const startDrag = (event, item) => {
+      console.log(item)
+      event.dataTransfer.dropEffect = 'move'
+      event.dataTransfer.effectAllowed = 'move'
+      event.dataTransfer.setData('itemID', item.id)
+    }
+
+    const onDrop = (event, list) => {
+      const itemID = event.dataTransfer.getData('itemID')
+      const item = items.value.find((item) => item.id == itemID)
+      item.list = list
+    }
+
+    const addList = () => {
+
+    }
+
+    return { getList, startDrag, onDrop }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Helvetica;
+    text-align: center;
+  }
+  .flexbox {
+    display: flex;
+  }
+  .dropzone {
+    width: 50%;
+    margin: 50px auto;
+    background-color: #bcdbff;
+    padding: 10px;
+    margin: 10px;
+    min-height: 10px;
+    border-radius: 10px;
+  }
+  .dropzone:last-child {
+    opacity: .5;
+  }
+  .drag-el {
+    background-color: #49b6ff;
+    color: white;
+    padding: 5px;
+    margin-bottom: 10px;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+  .drag-el:nth-last-of-type(1) {
+    margin-bottom: 0;
+  }
 </style>
